a0a7b829ae060c296874ca7e19fe07149817c1c7
Pierre Wacrenier <pierre@wacrenier.me>
2021-12-04 21:31:26 -0800

    2021 day2

---
+++ 2021/day2/p1/main.el
@@ -0,0 +1,33 @@
+#!/usr/bin/emacs --script
+
+(setq debug-on-error t)
+
+(setq pos 0)
+(setq depth 0)
+
+(defun filename-to-list (filename)
+  (split-string
+   (with-temp-buffer
+     (insert-file-contents filename)
+     (buffer-string))
+   "\n" t)
+  )
+
+(dolist (line (let ((filename (expand-file-name (car (last command-line-args)))))
+                (filename-to-list filename)
+                ))
+  (let ((instruction (split-string line)))
+    (let ((action (car instruction))
+          (value (string-to-number (car (cdr instruction))))
+          )
+      (pcase action
+        ("forward" (setq pos (+ pos value)))
+        ("down" (setq depth (+ depth value)))
+        ("up" (setq depth (- depth value)))
+        )
+      )
+    )
+  )
+
+(princ (* pos depth))
+(princ "\n")
